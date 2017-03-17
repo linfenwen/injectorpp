43a834e7f6a63f000e9ca6416c3e2309bce2c6d0
mazong1123 <mazong1123@gmail.com>
2017-03-17 22:24:17 +0800

    Roughly added skeleton of linux source code.

---
+++ linux/test/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Mar 17, 2017
+ *      Author: mazong1123
+ */
+
+#include <gmock/gmock.h>
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleMock(&argc, argv);
+
+    int result = RUN_ALL_TESTS();
+
+    return result;
+}
+
+
