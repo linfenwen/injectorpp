76516da037a03df12361ebae60f6e34f8ec20c7b
mazong1123 <mazong1123@gmail.com>
2017-03-17 22:41:47 +0800

    Updated format.

--- linux/test/main.cpp
+++ linux/test/main.cpp
@@ -9,11 +9,10 @@
 
 int main(int argc, char** argv)
 {
-    testing::InitGoogleMock(&argc, argv);
+	testing::InitGoogleMock(&argc, argv);
 
-    int result = RUN_ALL_TESTS();
+	int result = RUN_ALL_TESTS();
 
-    return result;
+	return result;
 }
-
 
