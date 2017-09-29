8a683655137e26fc696fbeee4b1682a20ad117b8
mazong1123 <mazong1123@gmail.com>
2017-09-29 14:48:44 +0800

    Updated command.

--- build.cmd
+++ build.cmd
@@ -1,5 +1,16 @@
-rmdir /s /q build
-md build
+@echo off
+if not "%~1"=="" (
+    set force=%1
+    if "%force%" == "/f" (
+        echo Deleting build folder...
+        rmdir build /s /q 
+    )
+)
+
+if not exist build (
+    echo Creating build folder...
+    md build
+)
 cd build
 cmake ../
 msbuild /nologo /t:injectorpp injectorpp.sln
